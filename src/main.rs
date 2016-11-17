bae556e971b324231ef85932c8e5341b023b59ce
Timidger <APragmaticPlace@gmail.com>
2016-11-16 19:44:20 -0500

    Got it memmapped, now protocol is failing...

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ extern crate memmap;
 
 use std::env;
 use std::process::exit;
-use cairo::{ImageSurface, Format};
+use cairo::{ImageSurface};
 //use gdk_sys;
 use memmap::Mmap;
 
@@ -79,7 +79,7 @@ impl Color {
 
 /// Given a solid color, writes bytes associated with that color to
 /// a special Wayland surface which is then rendered as a background for Way Cooler.
-pub fn generate_solid_background(color: Color, output: WlcOutput) {
+pub fn generate_solid_background(color: Color, _output: WlcOutput) {
     // Get shortcuts to the globals.
     let (display, iter) = get_display()
         .expect("Unable to connect to a wayland compositor");
@@ -128,23 +128,24 @@ pub fn generate_solid_background(color: Color, output: WlcOutput) {
 
 /// Given the data from an image, writes it to a special Wayland surface
 /// which is then rendered as a background for Way Cooler.
-pub fn generate_image_background(path: String, output: WlcOutput) {
+pub fn generate_image_background(path: String, _output: WlcOutput) {
     //let pix_buf = Pixbuf::new_from_file(path.as_str())
     //    .expect("Could not read the file");
-    let image_file = File::open(path.clone())
-    /*let image_file = ::std::fs::OpenOptions::new()
+    //let image_file = File::open(path.clone())
+    let image_file = ::std::fs::OpenOptions::new()
+        .read(true)
         .write(true)
         .create_new(false)
-        .open(path.clone())*/
+        .open(path.clone())
         .unwrap_or_else(|_| {
             println!("Could not open \"{:?}\"", path);
             panic!("Could not open image file");
         });
+    let raw_fd = image_file.as_raw_fd();
+    let map = Mmap::open(&image_file, memmap::Protection::ReadWrite)
+        .expect("Could not memory map the temp file");
     let background_surface = ImageSurface::create_from_png(image_file)
         .expect("could not create surface from png");
-    //let cairo_surface_from_pixbuf = gdk_sys::gdk_cairo_surface_create_from_pixbuf();
-    //let data = unsafe {pix_buf.get_pixels()};
-    //let data = read_image_data(path);
     // Get shortcuts to the globals.
     let (display, iter) = get_display()
         .expect("Unable to connect to a wayland compositor");
@@ -159,106 +160,37 @@ pub fn generate_image_background(path: String, output: WlcOutput) {
     let shell_surface = shell.get_shell_surface(&surface);
     // Calculate how big the buffer needs to be from the output resolution
     // TODO Get the output size from way cooler somehow...
-    //let resolution = output.get_resolution()
-    //    .expect("Couldn't get output resolution");
-    //let (width, height) = (resolution.w as i32, resolution.h as i32);
-    //let size = (width * height) as i32;
-    /*let resolution = output.get_resolution()
-        .expect("Couldn't get output resolution");
-    let (width, height) = (resolution.w as i32, resolution.h as i32);*/
-    //let size = width * height as i32;
 
 
     let width = background_surface.get_width();
     let height = background_surface.get_height();
     let stride = background_surface.get_stride();
-/*
-    let width = 800; let height = 600;
-    let stride = width * 4;
-*/
     let size: i32 = stride * height;
-    let mut tmp = tempfile::NamedTempFile::new().expect("Unable to create a tempfile.");
-    tmp.set_len(size as u64).expect("Could not truncate length of file");
-    let mut map = Mmap::open(&tmp, memmap::Protection::ReadWrite)
-        .expect("Could not memory map the temp file");
-    let mem_slice = unsafe { map.as_mut_slice() };
-    let boxed_mem: Box<[u8]> = mem_slice.to_vec().into_boxed_slice();
-
-    //let mut index = 0;
-    //println!("size of image (bytes): {:?}, rows: {:?}, height: {:?}, skip per row: {:?}",
-    //       pix_buf.get_byte_length(), pix_buf.get_width(), pix_buf.get_height(), pix_buf.get_rowstride());
-    //for index in 0..size as usize {
-    /*let mut counter = 0;
-    while index < size as usize {
-        if counter >= pix_buf.get_rowstride() / 4 {
-            for _ in 0..(width - counter) {
-                tmp.write_u32::<NativeEndian>(::std::u32::MAX)
-                    .expect("Could not write to temp file to generate image background");
-            }
-            counter = 0;
-        }
-        unsafe {
-            let bytes = if index < data.len() && index + 3 < data.len(){
-                let mut stuff: [u8; 4] = [0; 4];
-                // bgra
-                // eg, blue, green, red, something else
-                stuff[0] = data[index + 2];
-                stuff[1] = data[index + 1];
-                stuff[2] = data[index];
-                stuff[3] = data[index + 3];
-                //warn!("stuff: {:#?}", stuff);
-                transmute(stuff)
-            } else {
-                ::std::u32::MAX
-            };
-            tmp.write_u32::<NativeEndian>(bytes )
-                .expect("Could not write to temp file to generate image background");
-
-        }
-        index += 4;
-        counter += 1;
-    }
-    println!("pix buf stuff: width: {:?}, stride: {:?}", pix_buf.get_width(), pix_buf.get_rowstride());
-    tmp.flush()
-        .expect("Could not flush buffer");
-    */
+    println!("Size: {}, mmap len: {}", size, map.len());
+    let size = map.len() as i32;
+    //let tmp = tempfile::NamedTempFile::new().expect("Unable to create a tempfile.");
+    //tmp.set_len(size as u64).expect("Could not truncate length of file");
     // Create the buffer that is mem-mapped to the temp file descriptor
-    let pool = shm.create_pool(tmp.as_raw_fd(), size);
+    let pool = shm.create_pool(raw_fd, size);
     let buffer = pool.create_buffer(0, width, height, stride, WlShmFormat::Argb8888);
-    let cairo_surface = ImageSurface::create_for_data(boxed_mem, free_function, Format::ARgb32,
-                                                      width, height, stride);
-    let cairo_context = cairo::Context::new(&cairo_surface);
-    cairo_context.scale(1.0, 1.0);
-    cairo_context.set_source_surface(&background_surface, 0.0, 0.0);
-    // TODO Needed??
-    cairo_context.paint();
     // Tell Way Cooler not to put this in the tree, treat as background
+    // TODO Make this less hacky by actually giving way cooler access to this thing...
     shell_surface.set_class("Background".into());
     // TODO Actually give it the path or something idk
     shell_surface.set_title(format!("Image background yay"));
-    //let mut string = String::new();
-    /*for _ in 0..size {
-        unsafe {
-            tmp.write_u32::<NativeEndian>(6565656)
-                .expect("Could not write to file")
-        }
-    }*/
-    //tmp.read_to_string(&mut string).unwrap();
-    //println!("Buffer: {:?}", string);
 
     // Attach the buffer to the surface
     surface.attach(Some(&buffer), 0, 0);
+    surface.commit();
     surface.set_buffer_scale(1);
     surface.damage(0, 0, width, height);
 
+    println!("{:?}", unsafe{map.as_slice()});
     main_background_loop(compositor, shell, shm, seat, surface,
                          shell_surface, buffer, evt_iter);
 }
 
-fn free_function(_: Box<[u8]>) {
-    
-}
-
+#[allow(dead_code)]
 fn read_image_data(path: PathBuf) -> Vec<u32> {
     let image_file = File::open(path.clone())
         .unwrap_or_else(|_| {
@@ -306,6 +238,7 @@ fn main_background_loop(compositor: &WlCompositor, shell: &WlShell, shm: &WlShm,
     let mut pointer = seat.get_pointer();
     let cursor_theme = load_theme(None, 16, shm);
     let maybe_cursor = cursor_theme.get_cursor("default");
+    // TODO Uncomment
     /*if maybe_cursor.is_none() {
         println!("Could not load cursor theme properly, cannot load background");
         println!("Please consult the developers about this issue with your distro version, this is a known issue");
